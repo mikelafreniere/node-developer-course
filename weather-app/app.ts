c83cbfc5b5f805ea7ca19cf55f73ef1a4d2c998c
Mike LaFreniere <mlafreniere17@hotmail.com>
2021-12-25 11:20:11 -0700

    add getCurrentWeather util func

--- weather-app/app.ts
+++ weather-app/app.ts
@@ -1,11 +1,23 @@
-import * as weather from './weather';
+import * as util from './util';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-weather.geocode('Saskatoon', (error, data) => {
+util.geocode('Saskatoon', (error, data) => {
   if (!!error) {
     console.log(error.message);
     return;
   } else if (!!data) {
     console.log(data.latitude, data.longitude);
+    return;
+  }
+});
+
+util.getCurrentWeather(52.131802, -106.660767, (error, data) => {
+  if (!!error) {
+    console.log(error.message);
+    return;
+  } else if (!!data) {
+    console.log(data.temperature, data.feelsLike);
     return;
   }
 });
--- weather-app/util.ts
+++ weather-app/util.ts
@@ -1,29 +1,41 @@
 import * as request from 'request';
-import * as dotenv from 'dotenv';
-dotenv.config();
 
 export interface Geocode {
   latitude: string;
   longitude: string;
 }
 
+export interface Weather {
+  temperature: number;
+  feelsLike: number;
+}
+
 const WEATHERSTACK_URL = 'http://api.weatherstack.com';
 const MAPBOX_URL = 'https://api.mapbox.com';
 
-export function getCurrentWeather(query: string): void {
+export function getCurrentWeather(
+  latitude: number,
+  longitude: number,
+  callback: (error: Error | undefined, data: Weather | undefined) => void
+): void {
+  const query = encodeURIComponent(`${latitude},${longitude}`);
   const GET_WEATHER_URL = `${WEATHERSTACK_URL}/current?access_key=${process.env.WEATHERSTACK_API_KEY}&query=${query}`;
 
   request({ url: GET_WEATHER_URL, json: true }, (error, response) => {
     if (!!error) {
-      console.log('Something went wrong contacting weather service.');
+      callback(new Error('Unable to connect to the weather service.'), undefined);
       return;
     } else if (!!response.body?.error) {
-      console.log(response.body.error.info);
+      callback(new Error(`Unable to find '${query}'. Please try another search.`), undefined);
+      //   console.log(response.body.error.info);
       return;
     }
 
-    const current = response.body?.current || {};
-    console.log(`It is currently ${current.temperature} degrees out. It feels like ${current.feeslike} degrees out.`);
+    const current = response.body?.current || ({} as Weather);
+    callback(undefined, {
+      temperature: current.temperature,
+      feelsLike: current.feelslike,
+    });
   });
 }
 
@@ -31,13 +43,13 @@ export function geocode(
   address: string,
   callback: (error: Error | undefined, data: Geocode | undefined) => void
 ): void {
-  const encodedAddress = encodeURIComponent(address);
-  const url = `${MAPBOX_URL}/geocoding/v5/mapbox.places/${encodedAddress}.json?access_token=${process.env.MAPBOX_API_KEY}&limit=1`;
+  const url = `${MAPBOX_URL}/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${
+    process.env.MAPBOX_API_KEY
+  }&limit=1`;
 
   request({ url, json: true }, (error, response) => {
     if (!!error) {
-      console.log(error);
-      callback(new Error('Unable to connect to location services.'), undefined);
+      callback(new Error('Unable to connect to the location service.'), undefined);
       return;
     } else if (!!response.body?.message) {
       callback(new Error(`Unable to find '${address}'. Please try another search.`), undefined);
