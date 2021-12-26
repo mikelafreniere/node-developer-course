3b630379e27b17ffe07fc1a86b0607dbd7ed391e
Mike LaFreniere <mlafreniere17@hotmail.com>
2021-12-26 08:37:17 -0700

    use handlebars views

--- weather-app/src/app.ts
+++ weather-app/src/app.ts
@@ -6,53 +6,78 @@ import * as express from 'express';
 import { Geocode, Weather } from './util';
 
 dotenv.config();
-yargs.version('1.0.0');
 
-const app = express();
-app.use(express.static(path.join(__dirname, '../public')));
+const initExpress = () => {
+  const app = express();
+  // set path to serve static files
+  app.use(express.static(path.join(__dirname, '../public')));
+  // set location for frontend templates
+  app.set('views', path.join(__dirname, '../views'));
+  app.set('view engine', 'hbs');
 
-app.listen(3000, () => {
-  console.log('Server is running.');
-});
+  app.listen(3000, () => {
+    console.log('Server is running.');
+  });
 
-app.get('/weather', (req, res) => {
-  res.send({ forecast: 'forecast' });
-});
+  // routes
+  app.get('', (req, res) => {
+    res.render('index', { title: 'Weather' });
+  });
 
-yargs.command({
-  command: 'get',
-  describe: 'Get the current weather for a location',
-  builder: {
-    location: {
-      describe: 'The location to get the weather for',
-      demandOption: true,
-      type: 'string',
+  app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
+  });
+
+  app.get('/help', (req, res) => {
+    res.render('help', { title: 'Help' });
+  });
+
+  app.get('/weather', (req, res) => {
+    res.send({ forecast: 'forecast' });
+  });
+};
+
+const initYargs = () => {
+  yargs.version('1.0.0');
+
+  yargs.command({
+    command: 'get',
+    describe: 'Get the current weather for a location',
+    builder: {
+      location: {
+        describe: 'The location to get the weather for',
+        demandOption: true,
+        type: 'string',
+      },
     },
-  },
-  handler(argv: { location: string }): void {
-    if (!argv.location) {
-      console.log('Please specify a location.');
-      return;
-    }
-
-    util.geocode(argv.location, (error, geocodeData: Geocode) => {
-      if (error) {
-        console.log(error.message);
+    handler(argv: { location: string }): void {
+      if (!argv.location) {
+        console.log('Please specify a location.');
         return;
       }
 
-      const { latitude, longitude, placeName } = geocodeData;
-      util.getCurrentWeather(latitude, longitude, (error, data: Weather) => {
+      util.geocode(argv.location, (error, geocodeData: Geocode) => {
         if (error) {
           console.log(error.message);
           return;
         }
 
-        const { temperature, feelslike } = data;
-        console.log(`The temperature in ${placeName} is ${temperature} degrees. It feels like ${feelslike} degrees.`);
-      });
-    });
-  },
-});
+        const { latitude, longitude, placeName } = geocodeData;
+        util.getCurrentWeather(latitude, longitude, (error, data: Weather) => {
+          if (error) {
+            console.log(error.message);
+            return;
+          }
 
-yargs.parse();
+          const { temperature, feelslike } = data;
+          console.log(`The temperature in ${placeName} is ${temperature} degrees. It feels like ${feelslike} degrees.`);
+        });
+      });
+    },
+  });
+
+  yargs.parse();
+};
+
+initExpress();
+initYargs();
